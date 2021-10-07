e6ea954549c578f64f11a1aef24dde4fc5d81533
Mario Muñoz Fuentes <mario.munoz.fuentes>
2021-10-07 08:51:04 +0200

    Autor

---
+++ app/src/main/java/com/MarioMunozMyAplication/first/MainActivity.java
@@ -0,0 +1,17 @@
+package com.MarioMunozMyAplication.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author MarioMuñozFuentes
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
