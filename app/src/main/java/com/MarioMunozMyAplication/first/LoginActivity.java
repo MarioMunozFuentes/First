c4bafaf63ce2cac50ba2fc16f676119f2c177951
Mario Muñoz Fuentes <mario.munoz.fuentes>
2021-10-25 11:45:22 +0200

    implementacion de glide

--- app/src/main/java/com/MarioMunozMyAplication/first/LoginActivity.java
+++ app/src/main/java/com/MarioMunozMyAplication/first/LoginActivity.java
@@ -2,7 +2,9 @@ package com.MarioMunozMyAplication.first;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * @author MarioMuñozFuentes
@@ -14,4 +16,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void onClick(View v) {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
 }
+
