760cc1d04e60a9ab843588e52fc5cd8268b5ffd3
Mario Muñoz Fuentes <mario.munoz.fuentes>
2021-12-08 10:40:01 +0100

    ultimas correcciones1

--- app/src/main/java/com/MarioMunozMyAplication/first/LoginActivity.java
+++ app/src/main/java/com/MarioMunozMyAplication/first/LoginActivity.java
@@ -32,7 +32,6 @@ public class LoginActivity extends AppCompatActivity {
     }
 
 
-
     public void onClick(View v) {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         //con lo siguiente si lo ponemos no nos vuelve hacia atras
