945a091e84bfbc238e04a57cc909a96ad90eb744
Mario Muñoz Fuentes <mario.munoz.fuentes>
2022-01-20 10:03:43 +0100

    menubar casi terminado

--- app/src/main/java/com/MarioMunozMyAplication/first/MainBn.java
+++ app/src/main/java/com/MarioMunozMyAplication/first/MainBn.java
@@ -16,9 +16,21 @@ import android.view.View;
 import com.MarioMunozMyAplication.first.ui.main.SectionsPagerAdapter;
 import com.MarioMunozMyAplication.first.databinding.ActivityMainBnBinding;
 
+import android.widget.Toast;
+
+import androidx.annotation.IdRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainBn extends AppCompatActivity {
 
     private ActivityMainBnBinding binding;
+    private MenuItem prevMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,5 +54,81 @@ public class MainBn extends AppCompatActivity {
                 //        .setAction("Action", null).show();
             //}
     //    });
+
+
+
+
+    // cast al xml
+    BottomNavigationView mybottomNavView = findViewById(R.id.bottom_navigation);
+
+    // crear badges
+    //BottomNavigationMenuView bottomNavigationMenuView =
+    //        (BottomNavigationMenuView) mybottomNavView.getChildAt(0);
+    //View v = bottomNavigationMenuView.getChildAt(2);
+    //BottomNavigationItemView itemView = (BottomNavigationItemView) v;
+    //    LayoutInflater.from(this)
+    //            .inflate(R.layout.layout_badge, itemView, true);
+
+    //el listener del menu
+     mybottomNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+         @Override
+         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+             switch (item.getItemId()) {
+                 case R.id.heart:
+                     item.setChecked(true);
+                     Toast.makeText(MainBn.this, "Favourite clicked.", Toast.LENGTH_SHORT).show();
+                     //removeBadge(mybottomNavView,item.getItemId());
+                     viewPager.setCurrentItem(0);
+                     break;
+                 case R.id.search:
+                     item.setChecked(true);
+                     Toast.makeText(MainBn.this, "Add clicked.", Toast.LENGTH_SHORT).show();
+                     //removeBadge(mybottomNavView,item.getItemId());
+                     viewPager.setCurrentItem(1);
+                     break;
+                 case R.id.browse:
+                     item.setChecked(true);
+                     Toast.makeText(MainBn.this, "Browser clicked.", Toast.LENGTH_SHORT).show();
+                     //removeBadge(mybottomNavView,item.getItemId());
+                     viewPager.setCurrentItem(2);
+
+                     break;
+                 case R.id.profile:
+                     item.setChecked(true);
+                     Toast.makeText(MainBn.this, "Profile clicked.", Toast.LENGTH_SHORT).show();
+                     //removeBadge(mybottomNavView,item.getItemId());
+                     viewPager.setCurrentItem(3);
+                     break;
+             }
+             return false;
+         }
+    });
+
+     viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+         @Override
+         public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+         }
+
+         @Override
+         public void onPageSelected(int position) {
+             if (prevMenuItem != null){
+                 prevMenuItem.setChecked(false);
+             }
+             else{
+                 mybottomNavView.getMenu().getItem(0).setChecked(false);
+
+                 mybottomNavView.getMenu().getItem(position).setChecked(true);
+                 //removeBadge(mybottomNavView,mybottomNavView.getMenu().getItem(position).getItemId());
+                 prevMenuItem = mybottomNavView.getMenu().getItem(position);
+             }
+         }
+
+         @Override
+         public void onPageScrollStateChanged(int state) {
+
+         }
+     });
+
     }
 }
